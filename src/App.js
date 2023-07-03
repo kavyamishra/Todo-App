94177f48884df3617b0ac01355fec5665e8dcf5e
Kavya <kvymishra@gmail.com>
2023-07-03 11:50:51 +0530

    Project Todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodo([...todo, task]);
+    setTask("");
+  };
+
+  const removeTodo = (task) => {
+    const search = todo.filter((el) => el !== task);
+    setTodo(search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <form onSubmit={addTodo}>
+          <label htmlFor="task">Task</label>
+          <input
+            type="text"
+            name="task"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+      <div className="todos">
+        {todo.length > 0 ? (
+          todo.map((task, index) => (
+            <div key={index}>
+              <h3>{task}</h3>
+              <button onClick={() => removeTodo(task)}>Mark Done</button>
+            </div>
+          ))
+        ) : (
+          <p>No available tasks. Add some.</p>
+        )}
+      </div>
+    </>
   );
 }
 
